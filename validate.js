b9b01b6f1902a3163d3487270c1909820f14f8f3
Neeraj Thapa <neerajtp777@gmail.com>
2025-06-06 23:11:39 +0530

    fixed the mistaks

--- validate.js
+++ validate.js
@@ -1,55 +1,32 @@
-function validate()
-{
-
-
+function validate() {
   var name = document.getElementById('nameTxt').value;
-  var nameRGEX =  /^([a-zA-Z]){3,50}$/;
+  var nameRGEX = /^([a-zA-Z]){3,50}$/;
   var nameResult = nameRGEX.test(name);
 
   var phoneNumber = document.getElementById('contactTxt').value;
-  var phoneRGEX =  /^([0-9]){10}$/;
+  var phoneRGEX = /^([0-9]){10}$/;
   var phoneResult = phoneRGEX.test(phoneNumber);
 
   var email = document.getElementById('mailTxt').value;
-  var emailRGEX =  /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/
+  var emailRGEX = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
   var emailResult = emailRGEX.test(email);
 
-  var password=document.getElementById('passwordTxt').value;
-var passwordRGEX =  neeraj;
-var passresult=document.test(password)
+  var password = document.getElementById('passwordTxt').value;
+  var correctPassword = "neeraj"; // ✅ Define correct password as a string
 
-
-if(name!="" && phoneNumber!="" && email!="" && password!="neeraj"){
-
-
-  if(!nameResult)
-  {
-  	alert("INvalid name :Should be only character.Must be from 3 to 50 character" );
+  if (name !== "" && phoneNumber !== "" && email !== "" && password !== "") {
+    if (!nameResult) {
+      alert("Invalid name: Should be only characters, 3 to 50 characters long.");
+    } else if (!phoneResult) {
+      alert("Invalid phone number: Should be 10 digits.");
+    } else if (!emailResult) {
+      alert("Invalid email format.");
+    } else if (password !== correctPassword) {
+      alert("Wrong password.");
+    } else {
+      alert("Values Submitted");
+    }
+  } else {
+    alert("Fields should not be empty.");
   }
-  else if(!phoneResult)
-  {
-  	alert("INvalid phonenumber :Should be only number.Must be 10number" );
-  }
-  else if(!emailResult)  
-  {
- 	alert("INvalid email" );
-  }
-else if(!passresult)
-{
-alert("wrong password");
-}
-  else
-
-  {
- 	alert("Values Submitted"); 
-  }
- 
-  
-}
-else
-{
-alert("Fields should not be empty");
-}
-
-
 }
